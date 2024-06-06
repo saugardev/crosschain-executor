92077a76ec1aa0ac62f398f47901824487f79885
Saúl García Céspedes <saul@dixwe.com>
2024-06-06 15:00:49 +0200

    chore: remove comment

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,6 @@ export default function Home() {
       </button>
       <div className='mt-10'>
         {wallet === null ? null : <AcrossPlusComponent />}
-        {/* <AcrossEventListener /> */}
       </div>
     </main>
   )
